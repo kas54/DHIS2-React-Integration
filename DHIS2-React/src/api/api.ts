604ece9b6176ac8028d2a360e25b91b0a18e5af5
Kassahun Shimelis <kasushimelis54@gmail.com>
2026-02-02 09:46:07 +0300

    updating imported values

--- DHIS2-React/src/api/api.ts
+++ DHIS2-React/src/api/api.ts
@@ -1,16 +1,23 @@
 import axios from "axios";
 
+const BASE_URL = import.meta.env.VITE_DHIS2_BASE_URL;
+const USERNAME = import.meta.env.VITE_DHIS2_USERNAME;
+const PASSWORD = import.meta.env.VITE_DHIS2_PASSWORD;
+export const PROGRAM_ID = import.meta.env.VITE_PROGRAM_ID;
+export const ORG_UNIT_ID = import.meta.env.VITE_ORG_UNIT_ID;
+
+
 const api = axios.create({
-  baseURL: import.meta.env.VITE_DHIS2_BASE_URL,
+  baseURL: BASE_URL,
   auth: {
-    username: import.meta.env.VITE_DHIS2_USERNAME,
-    password: import.meta.env.VITE_DHIS2_PASSWORD,
+    username: USERNAME,
+    password: PASSWORD,
   },
 });
 
 export async function fetchProgram() {
   const res = await api.get(
-    `/api/programs/${import.meta.env.VITE_PROGRAM_ID}`,
+    `/api/programs/${PROGRAM_ID}`,
     {
       params: {
         fields:
@@ -25,8 +32,8 @@ export async function fetchProgram() {
 
 export async function createEvent(dataValues: any[]) {
   return api.post("/api/events", {
-    program: import.meta.env.VITE_PROGRAM_ID,
-    orgUnit: import.meta.env.VITE_ORG_UNIT_ID,
+    program: PROGRAM_ID,
+    orgUnit: ORG_UNIT_ID,
     eventDate: new Date().toISOString().slice(0, 10),
     dataValues,
   });
